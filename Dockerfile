2c167cee232838de07957364932814b370085ce8
dev01 <dev01@rksoftware.com.br>
2024-05-23 19:51:19 -0300

    install Bash on the Go image and create a gauge metric

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ WORKDIR /go/src
 ENV PATH="/go/bin:${PATH}"
 ENV CGO_ENABLED=0
 
+RUN apk add --no-cache bash
+
 CMD ["tail", "-f", "/dev/null"]
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "goapp_online_users",
+	Help: "Online users",
+	ConstLabels: map[string]string{
+		"site": "ecommerce",
+	},
+})
 
 func main() {
-	fmt.Println("Hello world")
+	r := prometheus.NewRegistry()
+	r.MustRegister(onlineUsers)
+
+	go func() {
+		for {
+			onlineUsers.Set(float64(rand.Intn(2000)))
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+
+	log.Fatal(http.ListenAndServe(":8181", nil))
 }
